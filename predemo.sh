fdf84981a33b27c7bcb5ecf1f49f64f7215686e0
disnocen <me@fadibarbara.it>
2023-07-26 20:58:13 +0200

    better workflow

--- predemo.sh
+++ predemo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "this is predemo.sh"
 
 a_addr=$(bitcoin-cli -rpcwallet=aliceWallet getnewaddress "" legacy)
 b_addr=$(bitcoin-cli -rpcwallet=bobWallet getnewaddress "" legacy)
@@ -14,5 +15,5 @@ echo "Alice's address: $a_addr and txhash: $a_txhash"
 echo "Bob's address: $b_addr and txhash: $b_txhash"
 echo "Carol's address: $c_addr and txhash: $c_txhash"
 
-bitcoin-cli -rpcwallet=testing generatetoaddress  105 bcrt1q3lcqyytfvfmkjxnkgp622rncnprsse523lcxr > /dev/null
+# bitcoin-cli -rpcwallet=testing generatetoaddress  105 bcrt1qkut7d6pwtj6t6lk768ldzggghnpt7ptx7scrjs > /dev/null
 echo "{'Alice':{'address':'$a_addr','txhash':'$a_txhash'},'Bob':{'address':'$b_addr','txhash':'$b_txhash'},'Carol':{'address':'$c_addr','txhash':'$c_txhash'}}" > demo_data.json
